c59eb87294a6b672b3bb6b7d8149a2643415e329
Philipp Melab <philipp.melab@gmail.com>
2018-09-17 13:49:20 +0200

    Store uuid instead of entity id.

--- src/Controller/MediaPreviewController.php
+++ src/Controller/MediaPreviewController.php
@@ -3,6 +3,7 @@
 namespace Drupal\sections\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Entity\EntityRepositoryInterface;
 use Drupal\Core\Render\RendererInterface;
 use Drupal\media\MediaInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -14,16 +15,24 @@ class MediaPreviewController extends ControllerBase {
    * @var \Drupal\Core\Render\RendererInterface
    */
   protected $renderer;
+
+  /**
+   * @var \Drupal\Core\Entity\EntityRepositoryInterface
+   */
+  protected $entityRepository;
+
   public static function create(ContainerInterface $container) {
-    return new static ($container->get('renderer'));
+    return new static ($container->get('renderer'), $container->get('entity.repository'));
   }
 
-  public function __construct(RendererInterface $renderer) {
+  public function __construct(RendererInterface $renderer, EntityRepositoryInterface $entityRepository) {
+    $this->entityRepository = $entityRepository;
     $this->renderer = $renderer;
   }
 
 
-  function preview(MediaInterface $media, $display) {
+  function preview($uuid, $display) {
+    $media = $this->entityRepository->loadEntityByUuid('media', $uuid);
     $build = $this->entityTypeManager()->getViewBuilder('media')->view($media, $display);
     $response = new Response();
     $response->setContent($this->renderer->render($build));
